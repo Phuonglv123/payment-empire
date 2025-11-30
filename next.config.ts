c58d1a7c7d271a484ac4cc08db3cb846e9ac0a9f
kai <vanphuong479@gmail.com>
2025-11-30 08:14:50 +0700

    feat(config): standardize quotes and add rewrites for backend API

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'api.qrserver.com',
+        protocol: "https",
+        hostname: "api.qrserver.com",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/backend/:path*", // Đường dẫn ảo trên Next.js
+        destination: "http://103.161.17.93/:path*", // Đường dẫn thật (HTTP)
+      },
+    ];
   },
 };
 
